7ac055eb9d94b1f20c9783c072ca77c40161481c
Tyson-Rodriguez <TysonRod17@gmail.com>
2016-09-29 13:44:27 -0700

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,8 @@
+// svae this file in a folder called java you have create in home
+// Hello2.java
+
+class Hello2 {
+    public static void main (String a[]){
+        System.out.print("Hello 2 world!");
+    }
+}        
---
+++ Program.java
@@ -0,0 +1,8 @@
+class Program {
+   public static void main(String[] args) {
+   
+   // Loop through 0, 1, 2, 3 and 4.
+   for (int i = 0; i < 5; i++) {
+   }
+   }
+}   
