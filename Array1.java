07c8e609f8bfbbd42812c8b2ed7f956f9e272b58
Tyson-Rodriguez <TysonRod17@gmail.com>
2016-10-13 11:55:55 -0700

    Update Array1.java

--- Array1.java
+++ Array1.java
@@ -1,29 +1,37 @@
 import java.util.Arrays;
 class Array1 {
-	public static void main (String a[]){
-		int i;
-		int[] ia ;ia = new  int[21];
-			ia[0] = -1;
-			ia[1] = 0;
-			ia[2] = -1;
-			ia[3] = 1;
-			ia[4] = 0;
-			ia[5] = 1;
-			ia[6] = -1;
-			ia[7] = 1;
-			ia[8] = -1;
-			ia[9] = 0;
-			
-			
-			System.out.println("Array unsorted\n");
-			for (i = 0; i <=11; i++){
-				System.out.print(ia[i]+" ");
-			}
-			System.out.print("\nArray sorted\n");
-			Arrays.sort(ia);
-			for (i = 0; i <=11; i++){
-				System.out.print(ia[i]+" ");
-			}
-			System.out.println();
-	}
+    public static void main (String a[]){
+   	 int i;
+   	 int[] ia ;ia =  new   int[21];
+   			 ia[0] = -2;
+   			 ia[1] = 0;
+   			 ia[2] = -1;
+   			 ia[3] = 1;
+   			 ia[4] = 0;
+   			 ia[5] = 1;
+   			 ia[6] = -1;
+   			 ia[7] = 1;
+   			 ia[8] = -1;
+   			 ia[9] = 0;
+   			 ia[10] = 1;
+   			 ia[11] = 2;
+   		 //print array
+   			 System.out.println("Array unsorted\n");
+   			 for (i = 0; i <=11; i++){
+   					 System.out.print(ia[i]+" ");
+   			 
+   			 }
+   		 // sort array
+   			 Arrays.sort(ia);
+   		 //print array
+   			 System.out.print("\nArray sorted\n");
+   			 
+   			 for (i = 0; i <=11; i++){
+   					 System.out.print(ia[i]+ " ");
+   					 
+   			 }
+   			 System.out.println();
+    }
 }
+
+
